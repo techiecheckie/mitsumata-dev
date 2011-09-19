809a285eb9e4a61e719f0b0f174e0f7b67ab717b
tapio.keranen@gmail.com <tapio.keranen@gmail.com@3b6bb96b-de8c-6681-8dfd-bbe4dcac8546>
2011-09-19 11:56:50 +0000

    Updated Nightly Options: Search. - locked and available (decision id) items only - random item placement

--- mitsugame/inventory.py
+++ mitsugame/inventory.py
@@ -11,6 +11,8 @@ class Inventory():
     # Debug counter
     unlock_count = 0
     
+    print "Initializing inventory..."
+    
     # Populates the items list, using the items defined in "items.xml"
     items_xml = xml.parse(renpy.loader.transfn("../items.xml"))
     item_elements = items_xml.findall("item")
@@ -18,16 +20,18 @@ class Inventory():
       id          = item_element.get("id")
       name        = item_element.find("name")
       description = item_element.find("description")
+      decisions   = item_element.find("decisions")
       
-      item = Item(id, name.text, description.text)
+      item = Item(id, name.text, description.text, decisions.text)
       
       if persistent_manager.has_item(id):
         item.unlock()
         unlock_count += 1
+        print "  Unlocked item", item.get_id()
         
       self.items.append(item)
       
-    print "Inventory initialized,", len(self.items), "items read into memory (%d items unlocked)" % unlock_count
+    print "  Done, ", len(self.items), "items read into memory (%d items unlocked)" % unlock_count
   
   def has_item(self, id):
     for item in self.items:
@@ -46,11 +50,21 @@ class Inventory():
       if item.get_id() == id:
         item.unlock()
         self.persistent_manager.add_item(id)
+        print "Inventory: unlocked item", item.get_id()
         return
     print "[WARN] Could not unlock item, no such id found (id: %s)" % id
   
   def get_items(self):
     return self.items
+    
+  def get_locked_items(self, decision_id):
+    locked_items = []
+    
+    for item in self.items:
+      if item.is_locked() and item.has_decision(decision_id):
+        locked_items.append(item)
+    
+    return locked_items
   
   def change_state(self):
     self.enabled = not self.enabled
